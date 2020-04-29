d172382a875015cd88d9ca9baf5ebbbbabffcecf
Justin McCaleb <justinleemccaleb@gmail.com>
2020-04-29 09:13:07 -0500

    adding IIFE

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,5 +1,8 @@
 "use strict";
 
+(function() {
+
+
 // 1. Create a file named break_and_continue.js in the js directory.
 // 2. Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting the user if they enter invalid input.
 // 3. Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
@@ -58,3 +61,4 @@
         }
         console.log('Here is an odd number: ' + i);
     }
+})();
--- js/for_loops.js
+++ js/for_loops.js
@@ -1,5 +1,7 @@
 "use strict";
 
+(function() {
+
 
 //Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)
 
@@ -63,3 +65,4 @@
     }
 
 
+})();
