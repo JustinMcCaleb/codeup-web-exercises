71bd081f0d17600093152a0d8138aa8baf0af63a
Justin McCaleb <justinleemccaleb@gmail.com>
2020-05-26 09:56:47 -0500

    completed promises exercise

--- js/promises.js
+++ js/promises.js
@@ -9,7 +9,24 @@
         .then(response => response.json())
         // .then(data => console.log(data))
 
-        .then(data => console.log(data[0].commit.author.date))
+        .then(data => console.log(data[0].commit.author.date));
             // - use with https://api.github.com/repos/JustinMcCaleb/codeup-web-exercises/commits to get last commit from codeup-web-exercises
+
+
+
+
+    function wait(number) {
+
+        const myPromise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                    resolve();
+            }, number);
+        });
+        return myPromise;
+    }
+
+    wait(1000).then(() => console.log('You will see this after 1 second'));
+    wait(3000).then(() => console.log('You will see this after 3 seconds'))
+
 
 })();
