8295bf31de99d560338d6ff02984a7b00bc3d02f
Justin McCaleb <justinleemccaleb@gmail.com>
2020-05-26 09:39:34 -0500

    completed first part of promises exercise

--- js/promises.js
+++ js/promises.js
@@ -2,9 +2,14 @@
 
 (function() {
 
-    fetch( 'https://api.github.com/users/JustinMcCaleb/events',
+    //https://api.github.com/users/JustinMcCaleb/repos
+
+    fetch( 'https://api.github.com/repos/JustinMcCaleb/codeup-web-exercises/commits',
         {headers: {'Authorization': `token ${GITHUB_TOKEN}`}})
         .then(response => response.json())
-        .then(data => console.log(data))
+        // .then(data => console.log(data))
+
+        .then(data => console.log(data[0].commit.author.date))
+            // - use with https://api.github.com/repos/JustinMcCaleb/codeup-web-exercises/commits to get last commit from codeup-web-exercises
 
 })();
