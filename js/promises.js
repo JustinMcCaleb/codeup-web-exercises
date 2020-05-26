4992c211e758195e5a89ecf257c45fe0893a2f61
Justin McCaleb <justinleemccaleb@gmail.com>
2020-05-25 21:24:37 -0500

    fetch request successful

--- js/promises.js
+++ js/promises.js
@@ -2,6 +2,9 @@
 
 (function() {
 
-
+    fetch( 'https://api.github.com/users/JustinMcCaleb/events',
+        {headers: {'Authorization': `token ${GITHUB_TOKEN}`}})
+        .then(response => response.json())
+        .then(data => console.log(data))
 
 })();
